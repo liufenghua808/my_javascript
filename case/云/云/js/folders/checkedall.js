4e7a2a84e4097b4fda7e694e601b16a2f8284d84
liufenghua808 <liufenghua3274@163.com>
2019-07-15 15:02:05 +0800

    delete case

--- case/云/云/js/folders/checkedall.js
+++
@@ -1,8 +0,0 @@
-checkedAll.onclick = function(){
-    let bool = checkedAll.classList.toggle('checked');
-    let ary = getChild(globalId);
-    ary.forEach(item=>{
-        item.checked = bool?true:false;
-    });
-    render(globalId);
-}
--- case/听写讲解/跨域/1,jsonp/server.js
+++
@@ -1,9 +0,0 @@
-let express = require('express');
-let app = express();
-
-app.get('/say',function(req,res){
-    let {wd,cb} = req.query;//查询字符串
-    console.log(wd);
-    res.end(`${cb}('No，Thanks')`);
-})
-app.listen(3002);
